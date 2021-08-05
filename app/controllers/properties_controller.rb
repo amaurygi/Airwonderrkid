b89ee404fa50bf405ef9989c27880f7ead008c94
rasovici <ivo.rasovic92@gmail.com>
2021-08-05 21:21:05 +0200

    property controller create update delete

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,5 @@
 class PropertiesController < ApplicationController
-  before_action :find_property, only: [:show]
+  before_action :find_property, only: [:show, :edit, :update, :destroy]
 
   def index
     @properties = Property.all
@@ -8,10 +8,45 @@ class PropertiesController < ApplicationController
   def show
   end
 
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    @property.user = current_user
+    if @property.save
+      redirect_to property_path(@property)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @property.update(property_params)
+      @property.user = current_user
+      redirect_to property_path(@property)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @property.delete
+    redirect_to properties_path
+  end
+
 
   private
 
   def find_property
     @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:name,:address,:description,:price)
   end
 end
